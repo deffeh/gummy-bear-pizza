79e879a65666fe1cdca7af41ef02ef91f3d47001
lekenneth77 <lekenneth77@gmail.com>
2024-09-28 11:19:49 -0500

    difficulty selector menu

---
+++ Assets/Scripts/Menus/DifficultyColumn.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public enum Difficulty {
+    Easy,
+    Standard,
+    Helldogger
+}
+
+
+public class DifficultyColumn : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Difficulty difficulty;
+    private const float notHoverOpacity = 0.9f;
+    private const float ANIM_DURATION = 0.5f;
+    // private Sequence seq;
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().DOFade(0, ANIM_DURATION);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<CanvasGroup>().DOFade(notHoverOpacity, ANIM_DURATION);
+    }
+
+    void Awake()
+    {
+        // seq = DOTween.Sequence();
+        // seq.SetEase(Ease.InOutQuad);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        GetComponent<Button>().onClick.AddListener(() => {
+
+        });
+    }
+
+    
+
+}
