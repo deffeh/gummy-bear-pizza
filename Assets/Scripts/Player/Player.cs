9f79ecff43885a360970b7c3073c966a68d30a43
Davie Nguyen <davie19nguyen@gmail.com>
2024-09-28 02:35:49 -0500

    added movement and jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,13 @@ public class Player : MonoBehaviour
 {
 
     public static Player Instance;
+    public Rigidbody PlayerRigidBody;
+
+    public float MovementSpeed;
+    public float AccelerationSpeed;
+    public float JumpSpeed;
+    public bool CanJump;
+    private Vector3 StrafeVelocity;
 
     void Awake() 
     {
@@ -28,6 +35,33 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float fallSpeed = PlayerRigidBody.velocity.y;
+        Vector3 targetVelocity = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            targetVelocity += Vector3.forward;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            targetVelocity += Vector3.back;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            targetVelocity += Vector3.left;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            targetVelocity += Vector3.right;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && CanJump)
+        {
+            fallSpeed = JumpSpeed;
+            CanJump = false;
+        }
+
+        targetVelocity.Normalize();
+        StrafeVelocity = Vector3.Lerp(StrafeVelocity, targetVelocity * MovementSpeed, AccelerationSpeed * Time.deltaTime);
+        PlayerRigidBody.velocity = new Vector3(StrafeVelocity.x, fallSpeed, StrafeVelocity.z);
     }
 }
