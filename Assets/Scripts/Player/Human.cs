4cad19632f7d57e2abae4d53d495319909c68483
Kaiden Zapanta <kaidenzapanta@gmail.com>
2024-09-29 17:01:49 -0500

    added human catch up mechanic

--- Assets/Scripts/Player/Human.cs
+++ Assets/Scripts/Player/Human.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -17,7 +18,8 @@ public class Human : MonoBehaviour
     public int CurrentHealth;
     public int MaxHealth;
 
-    private Player Player;
+    private Player player;
+    public float maxDistFromPlayer = 35f;
     public HumanState CurrentState;
 
     public Rigidbody HumanRigidBody;
@@ -27,6 +29,7 @@ public class Human : MonoBehaviour
     public Animator anim;
     public LineRenderer lr;
     public float lineSize = 1f;
+    private bool catchingUpToPlayer;
 
 
     // real shit?
@@ -37,16 +40,20 @@ public class Human : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        else{
+        else 
+        {
             Instance = this;
         }
+
+        catchingUpToPlayer = false;
+        CurrentHealth = MaxHealth;
     }
     
     // Start is called before the first frame update
     void Start()
     {
         CurrentState = HumanState.Idle;
-        Player = Player.Instance;
+        player = Player.Instance;
         lr.startWidth = lineSize;
         lr.endWidth = lineSize;
         lr.positionCount = 0;
@@ -57,12 +64,21 @@ public class Human : MonoBehaviour
     {
         if (HumanNavMeshAgent.remainingDistance <= HumanNavMeshAgent.stoppingDistance) {
             anim.Play("HumanIdle");
+            catchingUpToPlayer = false;
             lr.positionCount = 0;
         } else {
             Vector3[] pathCorners = HumanNavMeshAgent.path.corners;
             lr.positionCount = pathCorners.Length;
             lr.SetPositions(pathCorners);
         }
+
+        float distFromPlayer = (player.transform.position - transform.position).magnitude;
+        if (distFromPlayer > maxDistFromPlayer && !catchingUpToPlayer)
+        {
+            catchingUpToPlayer = true;
+            MoveTo(player.transform.position);
+        }
+        
     }
 
     void FixedUpdate()
@@ -70,7 +86,7 @@ public class Human : MonoBehaviour
         switch(CurrentState)
         {
             case HumanState.ReloadingNav:
-            MoveTo(Player.transform.position);
+            MoveTo(player.transform.position);
             break;
         }
     }
@@ -90,7 +106,7 @@ public class Human : MonoBehaviour
     public void Reload()
     {
         CurrentState = HumanState.Reloading;
-        Player.Reload();
+        player.Reload();
     }
 
     public void OnHeal(int heal) {
@@ -101,8 +117,8 @@ public class Human : MonoBehaviour
     public void OnHit(int damage) {
         CurrentHealth = Math.Max(0, CurrentHealth - damage);
         if (CurrentHealth == 0) {
-            //unborn yourself
-        } else {
+            PauseMenu.Instance.Die();
+            return;
         }
         OnTakeDamage?.Invoke(CurrentHealth);
     }
